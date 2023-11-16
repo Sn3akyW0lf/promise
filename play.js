d6b059ac2ead388765ea44f1564cf7180e5d153c
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-16 15:58:11 +0530

    js refresher 1

---
+++ play.js
@@ -0,0 +1,12 @@
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+
+
+
+function summarizeUser (userName, userAge, userHasHobbies) {
+    return `Name is ${userName}, age ${userAge} and user has hobbies: ${userHasHobbies}`;
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
+
