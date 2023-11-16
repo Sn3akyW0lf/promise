d1400858e1091d64660c1fc32fc27d3197c6bb31
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-16 16:15:45 +0530

    added objects

--- play.js
+++ play.js
@@ -1,17 +1,38 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
+// const name = 'Max';
+// let age = 29;
+// const hasHobbies = true;
 
-// name = 'Tony';
-age = 35;
+// // name = 'Tony';
+// age = 35;
 
-const summarizeUser = (userName, userAge, userHasHobbies) => {
-    return `Name is ${userName}, age ${userAge} and user has hobbies: ${userHasHobbies}`;
-}
+// const summarizeUser = (userName, userAge, userHasHobbies) => {
+//     return `Name is ${userName}, age ${userAge} and user has hobbies: ${userHasHobbies}`;
+// }
 
-const prod = (a, b) => a * b;
+// const prod = (a, b) => a * b;
 
-console.log('Product: ' + prod(4, 8));
+// console.log('Product: ' + prod(4, 8));
 
-console.log(summarizeUser(name, age, hasHobbies));
+// console.log(summarizeUser(name, age, hasHobbies));
 
+
+const person = {
+    name: 'Tony',
+    age: 45,
+    greet () {
+        console.log(`Hello, this is ${this.name}`);
+    }
+};
+
+console.log(person);
+person.greet()
+
+const student = {
+    name: 'Siddhesh',
+    age: 25,
+    intro () {
+        console.log(`Hello, I am ${this.name} and I am ${this.age}`);
+    }
+};
+
+student.intro();
