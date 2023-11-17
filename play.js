578bd9c128dc387c988914df1a4f8260c57c571c
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-17 10:54:23 +0530

    spread and rest operators

--- play.js
+++ play.js
@@ -1,13 +1,13 @@
-const person = {
-    name: 'Tony',
-    age: 45,
-    greet () {
-        console.log(`Hello, this is ${this.name}`);
-    }
-};
+// const person = {
+//     name: 'Tony',
+//     age: 45,
+//     greet () {
+//         console.log(`Hello, this is ${this.name}`);
+//     }
+// };
 
-console.log(person);
-person.greet()
+// console.log(person);
+// person.greet()
 
 const student = {
     name: 'Siddhesh',
@@ -17,18 +17,31 @@ const student = {
     }
 };
 
-student.intro();
+// student.intro();
 
-const array = ['apple', 'oranges', '', 'mango', '', 'lemon'];
+const copyStudent = {...student};
+console.log(copyStudent)
+
+const fruits = ['apple', 'oranges', '', 'mango', '', 'lemon'];
 
 // for (let ele of array) {
 //     console.log(ele);
 // }
 
-console.log(array.map(ele => {
-    if (ele === '') {
+console.log(fruits.map(fruit => {
+    if (fruit === '') {
         return 'empty string';
     }
-    return ele;
+    return fruit;
 }));
-console.log(array)
+console.log(fruits)
+
+const copyFruits = [...fruits];
+console.log(copyFruits);
+
+
+const toArray = (...args) => {
+    return args;
+}
+
+console.log(toArray(2, 4, 8, 16));
