6313153b01b2877c9adf52c4c8d1c7be24463764
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-19 22:31:32 +0530

    responding a html page with conditions

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ const http = require('http');
 // }
 
 const server = http.createServer((req, res) => {
-    console.log('Siddhesh Meher');
+    // console.log(req.url, req.method, req.headers);
+    // console.log('Siddhesh Meher');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html');
+    res.write('<head><title>My First Page</title></head>');
+    if (req.url == '/home') {
+        // console.log('Welcome home');
+        res.write('<body><h1>Welcome Home</body>');
+    } else if (req.url == '/about') {
+        res.write('<body><h1>Welcome to About Us page</body>');
+    } else if (req.url == '/node') {
+        res.write('<body><h1>Welcome to my Node.JS Project</body>');        
+    }
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(4000);
