218ec03b996bb15147a9da5c0c470ea67800a7a6
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-19 10:05:17 +0530

    setting up a server and trying out simple things

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+// function rqListener (req, res) {
+
+// }
+
+const server = http.createServer((req, res) => {
+    console.log('Siddhesh Meher');
+});
+
+server.listen(4000);
