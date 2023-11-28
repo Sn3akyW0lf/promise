6d931c637bfb04fc60bf1100184f6f7f340a4bea
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-28 13:32:30 +0530

    added body parser module and added a form in the project, which returns the data filled and prints it in the console.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware.');
-    next(); // This function allows the request to continue to next middleware in line
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+    // console.log('in product middleware.');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('in another middleware.');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    // console.log('in another middleware.');
     res.send('<h1> Hello from Express</h1>');
 });
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
 
 app.listen(3000);
