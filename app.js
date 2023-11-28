206f575c7380a5a20158976680064c5027683243
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-28 18:03:49 +0530

    Cleaned up the Code and added routing to the application

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    // console.log('in product middleware.');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use('/shop', shopRoutes);
 
-app.use('/', (req, res, next) => {
-    // console.log('in another middleware.');
-    res.send('<h1> Hello from Express</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found :(');
 });
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+// /admin/add-product => GET req
+router.get('/add-product', (req, res, next) => {
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+});
+
+// /admin/add-product => POST req
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/shop/');
+});
+
+module.exports = router;
