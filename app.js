7884a39e87fbfb520fb8e33f7d5e68ba2005bb0c
Sn3akyW0lf <siddhesh.meher@protonmail.com>
2023-11-29 23:20:48 +0530

    beautified the wesite and added some more pages to to make it more lively

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,15 +6,22 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact-us');
+const successRoutes = require('./routes/success')
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
 app.use('/shop', shopRoutes);
 
+app.use('/contact-us', contactRoutes);
+
+app.use('/success', successRoutes);
+
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found :(');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
